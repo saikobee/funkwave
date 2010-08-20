c25c4bc441d0a9b929633ca4dbb22ded65307347
mock.brian <mock.brian@a74cb6f4-7b5b-a057-7012-1eb00cd0a10e>
2010-08-20 09:51:03 +0000

    Added small class to assist with pixel-perfect sprite placement

---
+++ intsprite.py
@@ -0,0 +1,12 @@
+import rabbyt
+
+class IntSprite(rabbyt.Sprite):
+    xf  = rabbyt.anim_slot()
+    yf  = rabbyt.anim_slot()
+    xyf = rabbyt.swizzle("xf", "yf")
+    
+    @staticmethod
+    def truncate(sprites):
+        for sprite in sprites:
+            sprite.x = int(sprite.xf)
+            sprite.y = int(sprite.yf)
