8d91cc1c9c5f5560f2f38d3d9c570fbc81293022
mock.brian <mock.brian@a74cb6f4-7b5b-a057-7012-1eb00cd0a10e>
2010-08-21 07:25:04 +0000

    Added an incomplete ParaFactory and documentation

--- trunk/intsprite.py
+++ trunk/intsprite.py
@@ -1,20 +1,36 @@
 import rabbyt
 
 class IntSprite(rabbyt.Sprite):
+    '''\
+    This class manages a floating point location for a sprite while
+    giving integer locations to the rendering engine so we don't get
+    blurry sprites centered at locations like (1.333, 3.14159).
+    '''
+
     xf  = rabbyt.anim_slot()
     yf  = rabbyt.anim_slot()
     xyf = rabbyt.swizzle("xf", "yf")
     
     @staticmethod
     def truncate(*sprites):
+        '''\
+        Like truncate_coords, but operates on a slurpy list of args.
+        '''
         for sprite in sprites:
             sprite.x = int(sprite.xf)
             sprite.y = int(sprite.yf)
 
     @staticmethod
     def truncate_list(sprites):
+        '''\
+        Like truncate_coords, but operates on a list of args.
+        '''
         self.truncate(*sprites)
 
     def truncate_coords(self):
+        '''\
+        Sets the x and y values to the integer portions of their floating
+        point counterparts, xf and yf.
+        '''
         self.x = int(self.xf)
         self.y = int(self.yf)
--- trunk/playersprite.py
+++ trunk/playersprite.py
@@ -3,6 +3,10 @@ import rabbyt
 from intsprite import IntSprite
 
 class PlayerSprite(IntSprite):
+    '''\
+    This class helps to manage the tricky business of using a keyboard
+    to determine the velocity of a player.
+    '''
     v_max = 100
 
     x_keys = [0]
@@ -16,12 +20,15 @@ class PlayerSprite(IntSprite):
     def vx(self): return self.x_keys[-1] * self.v_max
     def vy(self): return self.y_keys[-1] * self.v_max
 
-    def pop_left(self):  self.x_keys.remove(-1)
-    def pop_right(self): self.x_keys.remove( 1)
-    def pop_up(self):    self.y_keys.remove( 1)
-    def pop_down(self):  self.y_keys.remove(-1)
+    def stop_left(self):  self.x_keys.remove(-1)
+    def stop_right(self): self.x_keys.remove( 1)
+    def stop_up(self):    self.y_keys.remove( 1)
+    def stop_down(self):  self.y_keys.remove(-1)
 
     def update(self, dt):
+        '''\
+        Simply update the position based on the current velocities.
+        '''
         self.xf += self.vx() * dt
         self.yf += self.vy() * dt
         self.truncate_coords()
