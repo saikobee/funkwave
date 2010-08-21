a5c2892b9d4b74771acb3dd951c7ffe3f136fca8
mock.brian <mock.brian@a74cb6f4-7b5b-a057-7012-1eb00cd0a10e>
2010-08-21 00:25:20 +0000

    Added some functions and fixed indentation

--- intsprite.py
+++ intsprite.py
@@ -6,9 +6,11 @@ class IntSprite(rabbyt.Sprite):
     xyf = rabbyt.swizzle("xf", "yf")
     
     @staticmethod
-    def truncate(sprites):
+    def truncate(*sprites):
         for sprite in sprites:
             sprite.x = int(sprite.xf)
             sprite.y = int(sprite.yf)
-            #sprite.x = sprite.xf
-            #sprite.y = sprite.yf
+
+    @staticmethod
+    def truncate_list(sprites):
+        self.truncate(*sprites)
