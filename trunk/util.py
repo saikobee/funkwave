b69d5925bef322883b4ac267d65912c286e57563
mock.brian <mock.brian@a74cb6f4-7b5b-a057-7012-1eb00cd0a10e>
2010-08-25 00:39:03 +0000

    Added keybinds, cleaned up code, added docstrings

--- trunk/util.py
+++ trunk/util.py
@@ -1,15 +1,19 @@
 class Point(object):
+    '''Turns an (x, y) tuple into an object with .x and .y'''
     def __init__(point):
         self.x, self.y = point
 
 class RelPoint(object):
+    '''RelPoint is a point relative to another point, offset by (h, k)'''
     def __init__(self, sprite, h, k):
         self.sprite = sprite
         self.h = h
         self.k = k
-    @property # hax to hide function calls
+
+    @property
     def x(self):
         return self.sprite.x + self.h
-    @property # hax to hide function calls
+
+    @property
     def y(self):
         return self.sprite.y + self.k
