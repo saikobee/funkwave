7e31561ec97aa123588d3a82e41b20faa6fd6fa3
mock.brian <mock.brian@a74cb6f4-7b5b-a057-7012-1eb00cd0a10e>
2010-08-25 02:13:19 +0000

    'Fixed' shot inputs

--- playersprite.py
+++ playersprite.py
@@ -2,6 +2,7 @@ import pyglet
 import rabbyt
 
 from intsprite import IntSprite
+from shots import Shot1, Shot2
 
 class PlayerSprite(IntSprite):
     '''\
@@ -21,6 +22,25 @@ class PlayerSprite(IntSprite):
 
         self.x_keys = [0]
         self.y_keys = [0]
+
+        self.shots = (Shot1(self), Shot2(self))
+
+        self.shot_keys = [None]
+
+    def try_shot1(self): self.shot_keys.append(0)
+    def try_shot2(self): self.shot_keys.append(1)
+
+    def stop_shot1(self):
+        if 0 in self.shot_keys:
+            self.shot_keys.remove(0)
+
+        self.shots[0].pause()
+
+    def stop_shot2(self):
+        if 1 in self.shot_keys:
+            self.shot_keys.remove(1)
+
+        self.shots[1].pause()
 
     def go_left(self):  self.x_keys.append(-1)
     def go_right(self): self.x_keys.append( 1)
@@ -49,3 +69,20 @@ class PlayerSprite(IntSprite):
         self.xf += self.vx() * dt
         self.yf += self.vy() * dt
         self.truncate_coords()
+
+        i = self.shot_keys[-1]
+        if i is not None:
+            ## This section of code ensures pressing other buttons
+            ## doesn't change shot type
+            #if all(map(lambda f: f.paused(), self.shots)):
+            #    self.shots[i].play()
+
+            ## This section of code allows other buttons to change
+            ## the current shot type
+            if self.shots[i].paused():
+                for shot in self.shots:
+                    shot.pause()
+                self.shots[i].play()
+
+        for shot in self.shots:
+            shot.update(dt)
