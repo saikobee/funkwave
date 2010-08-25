537dc6d4d79bb5aa3e821ff0ef9b999bb34dc8e4
fish.ash <fish.ash@a74cb6f4-7b5b-a057-7012-1eb00cd0a10e>
2010-08-25 00:27:24 +0000

    Created a focused shot

--- shots.py
+++ shots.py
@@ -1,4 +1,5 @@
 from factories import LineFactory
+from util      import RelPoint
 
 class Shot1:
     def __init__(self, sprite):
@@ -18,7 +19,7 @@ class Shot1:
 
     def spawn_bullet(self, dt):
         for factory in self.factories:
-            factory.spawn_bullet(1.0/self.spawn_rate)
+            factory.spawn_bullet(dt)
 
     def update(self, dt):
         for factory in self.factories:
@@ -35,3 +36,41 @@ class Shot1:
     def toggle(self):
         for factory in self.factories:
             factory.toggle()
+
+
+class Shot2:
+    def __init__(self, sprite):
+        self.sprite = sprite
+
+        self.spawn_rate =  15
+        self.age_factor = 512 + 128
+        
+        self.offsets = [10, -10]
+
+        self.factories = [LineFactory(
+                0,
+                RelPoint(self.sprite, 0, offset),
+                self.spawn_rate,
+                self.age_factor
+        ) for offset in self.offsets]
+
+    def spawn_bullet(self, dt):
+        for factory in self.factories:
+            factory.spawn_bullet(dt)
+
+    def update(self, dt):
+        for factory in self.factories:
+            factory.update(dt)
+
+    def pause(self):
+        for factory in self.factories:
+            factory.pause()
+
+    def play(self):
+        for factory in self.factories:
+            factory.play()
+
+    def toggle(self):
+        for factory in self.factories:
+            factory.toggle()
+
